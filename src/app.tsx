47762075a2a47d4cad41f55acc2fedd6841f6ac3
manudous <manudous@gmail.com>
2023-08-08 10:40:46 +0200

    add listados

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,28 @@
+import React from "react";
+import { Miembro } from "./modelo";
+
 export const App = () => {
+  const [miembros, setMiembros] = React.useState<Miembro[]>([]);
+
+  React.useEffect(() => {
+    fetch(`https://api.github.com/orgs/lemoncode/members`)
+      .then((response) => response.json())
+      .then((json) => setMiembros(json))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
-    <>
-      <h1>Bootcamps JS - TypeScript React SandBox</h1>
-    </>
+    <div className="user-list-container">
+      <span className="header">Avatar</span>
+      <span className="header">Id</span>
+      <span className="header">Nombre</span>
+      {miembros.map((miembro) => (
+        <React.Fragment key={miembro.id}>
+          <img src={miembro.avatar_url} />
+          <span>{miembro.id}</span>
+          <span>{miembro.login}</span>
+        </React.Fragment>
+      ))}
+    </div>
   );
 };
